30c5e6b4de62357a9c2a4bf2ad3c1d7980204aec
Anand Kumar Ray <40709301+akray1999@users.noreply.github.com>
2021-01-15 13:44:41 +0530

    Update server.js

--- signupbackend/server.js
+++ signupbackend/server.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 
 dotenv.config()
 
-mongoose.connect(process.env.DATABASE_ACCESS, {useNewUrlParser: true } ,()=>{
+mongoose.connect(process.env.DATABASE_ACCESS, ()=>{
    
         console.log("Database connected");
    
